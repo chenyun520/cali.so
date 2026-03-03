06af0cf957d033c119648b9cf318236e94f36f45
chenyun520 <135810487+chenyun520@users.noreply.github.com>
2026-03-03 13:44:49 +0800

    fix: import structureTool from sanity instead of sanity/structure

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,12 +1,11 @@
 'use client'
 
 /**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...tool]]\page.tsx` route
+ * This configuration is used to for the Sanity Studio that's mounted on the `\app\studio\[[...tool]]\page.tsx` route
  */
 
 import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import {defineConfig, structureTool} from 'sanity'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './sanity/env'
